004e8908ea3b48ea04b2ee7d8f4de6cee90803f7
odarmokhval <olga.darmokhval@gmail.com>
2017-11-28 09:34:11 +0200

    comment

---
+++ odarmokhval/Variables1/Variables1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Variables1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            //Console.WriteLine("х equel " + х);
+            x = -1 + 3 * 5;            
+            Console.Write("x = -1 + 3 * 5 = ");
+            Console.WriteLine(x);
+
+
+            x = (24 + 5) % 7;
+            Console.Write("x = (24 + 5) mod 7 = ");
+            Console.WriteLine(x);
+
+            x = 15 + -4 * 6 / 11;
+            Console.Write("x = 15 + -4 * 6 / 11 =  ");
+            Console.WriteLine(x);
+
+            x = 2 + 10 / 6 * 1 - 7 % 2;
+            Console.Write("x = 2 + 10 / 6 * 1 - 7 mod 2 =  ");
+            Console.WriteLine(x);            
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ odarmokhval/Variables2/Variables2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Variables2
+{
+    class Variables2
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            x = 12;
+            y = 3;
+
+            Console.WriteLine("x equals " + x);
+            Console.WriteLine("y equals " + y);
+            z = x + y;
+            Console.WriteLine("z equals x+y:");
+            Console.WriteLine (z);
+            
+
+            z = x - y;
+            Console.WriteLine("z equals x-y:");
+            Console.WriteLine(z);
+            
+
+            z = x * y;
+            Console.WriteLine("z equals x*y:");
+            Console.WriteLine(z);
+            
+
+            z = x / y;
+            Console.WriteLine("z equals x/y:");
+            Console.WriteLine(z);
+            
+            z = x % y;
+            Console.WriteLine("z equals x mod y:");
+            Console.WriteLine(z);
+            Console.ReadKey();
+        }
+    }
+}
