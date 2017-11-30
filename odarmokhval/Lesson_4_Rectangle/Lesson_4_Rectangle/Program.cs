8f54cad0b1413c7ee930cfcb15b93a931a05d352
odarmokhval <olga.darmokhval@gmail.com>
2017-11-30 22:12:52 +0200

    3

---
+++ odarmokhval/Lesson_4_Rectangle/Lesson_4_Rectangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Lesson_4_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Enter a number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("{0}{0}{0}", x);
+            Console.WriteLine("{0} {0}", x);
+            Console.WriteLine("{0} {0}", x);
+            Console.WriteLine("{0} {0}", x);
+            Console.WriteLine("{0}{0}{0}", x);
+
+            Console.ReadKey();
+        }
+    }
+}
