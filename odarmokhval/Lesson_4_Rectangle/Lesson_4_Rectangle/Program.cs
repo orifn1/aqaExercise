b2f15ce2f01d453b14afe96b547fd6c5ac3c8882
yaksyutin <aksyutin_yurij@outlook.com>
2017-11-30 22:40:11 +0200

    for right paint rectangl

--- odarmokhval/Lesson_4_Rectangle/Lesson_4_Rectangle/Program.cs
+++ odarmokhval/Lesson_4_Rectangle/Lesson_4_Rectangle/Program.cs
@@ -12,10 +12,12 @@ namespace Lesson_4_Rectangle
             x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
 
+            int n = x.ToString().Length;
+
             Console.WriteLine("{0}{0}{0}", x);
-            Console.WriteLine("{0} {0}", x);
-            Console.WriteLine("{0} {0}", x);
-            Console.WriteLine("{0} {0}", x);
+            Console.WriteLine("{0}{1}{0}", x, new String(' ', n));
+            Console.WriteLine("{0}{1}{0}", x, new String(' ', n));
+            Console.WriteLine("{0}{1}{0}", x, new String(' ', n));
             Console.WriteLine("{0}{0}{0}", x);
 
             Console.ReadKey();
