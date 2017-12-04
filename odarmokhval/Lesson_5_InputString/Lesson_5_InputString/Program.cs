7dd0912a51eb13dfa1b57cdd4e1f707481f8ce28
odarmokhval <olga.darmokhval@gmail.com>
2017-12-04 11:18:09 +0200

    5 lesson, 1-st version

---
+++ odarmokhval/Lesson_5_InputString/Lesson_5_InputString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Lesson_5_InputString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int x;
+            x = 0;
+
+            Console.Write("Enter the characters you want to reverse: ");
+            str = Console.ReadLine();
+
+            x = str.Length - 1;
+            Console.Write("The characters in reverse order:  ");
+            while (x >= 0)
+            {
+                Console.Write("{0} ", str[x]);
+                x--;
+
+            }
+                Console.ReadKey();
+        }
+    }
+}
