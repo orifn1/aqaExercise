48f48fa4dcecb7013cf50af86c98401fb4a4aebf
Poliakova <makovej@gmail.com>
2017-12-14 16:56:29 +0200

    1

---
+++ kpoliakova/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
